6c2d18a5893455f087d655af0536bef5ccdfe867
qiyaozu <1640644790@qq.com>
2019-02-22 16:10:56 +0800

    change

--- src/api/index.js
+++ src/api/index.js
@@ -14,16 +14,7 @@ class ApiController {
    * @return {[Object]}            返回请求响应
    */
   async getcode (postData = {}) {
-    let res = await api.post('send-mobile-code/new-user', postData, this.reqAuthorization)
-    return res
-  }
-
-  /**
-   * 下载统计
-   * @return {[Object]}            返回请求响应
-   */
-  async downCount (way) {
-    let res = await api.get('download-statistics?way=' + way)
+    let res = await api.post('send-mobile-code', postData, this.reqAuthorization)
     return res
   }
 
@@ -33,53 +24,6 @@ class ApiController {
    */
   async activityConf (params) {
     let res = await api.get('activity-conf', params)
-    return res
-  }
-
-  /**
-   * 获取排行列表
-   * @return {[Object]}            返回请求响应
-   */
-  async getActivityRank (pageIndex) {
-    let res = await api.get('corps-rank?pn=' + pageIndex)
-    return res
-  }
-
-  /**
-   * 获取战队详情
-   * @return {[Object]}            返回请求响应
-   */
-  async getTeamDetail (mobile) {
-    let res = await api.get('corps-detail?mobile=' + mobile)
-    return res
-  }
-
-  /**
-   * 组建战队
-   * @param  {[Object]} postData 请求数据 { }
-   * @return {[Object]}            返回请求响应
-   */
-  async buildTeam (postData = {}) {
-    let res = await api.post('initiate-corps', postData)
-    return res
-  }
-
-  /**
-   * 获取空投排行列表
-   * @return {[Object]}            返回请求响应
-   */
-  async getKongtouRank (pageIndex) {
-    let res = await api.get('snapshot-rank?pn=' + pageIndex)
-    return res
-  }
-
-  /**
-   * 注册
-   * @param  {[Object]} postData 请求数据 { mobile: '', code: ''... }
-   * @return {[Object]}            返回请求响应
-   */
-  async regist (postData = {}) {
-    let res = await api.post('activity-sign-up', postData)
     return res
   }
 }
