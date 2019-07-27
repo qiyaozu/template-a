ec783b07d387b5550a2e5629d7bebaef186ecef0
qiyaozu <1640644790@qq.com>
2019-07-27 20:09:37 +0800

    change index.html

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,22 @@
-const SentryCliPlugin = require("@sentry/webpack-plugin");
 const settings = require('./src/settings')
 module.exports = {
-    configureWebpack: {
-      plugins: [
-        new SentryCliPlugin({
-            include: ".",
-            release: settings.RELEASE,
-            ignore: ['node_modules'],
-            configFile: ".sentryclirc"
-        })
-      ]
-    }
+  configureWebpack: {
+    plugins: check_env_to_gene_plugin()
+  }
+}
+
+function check_env_to_gene_plugin() {
+  if (process.env.NODE_ENV === 'development') {
+    return []
+  } else {
+    const SentryCliPlugin = require("@sentry/webpack-plugin");
+    return [
+      new SentryCliPlugin({
+        include: ".",
+        release: settings.RELEASE,
+        ignore: ['node_modules'],
+        configFile: ".sentryclirc"
+      })
+    ]
+  }
 }
