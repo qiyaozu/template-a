418ff97b5b791be62225527b934c2409335664aa
qiyaozu <1640644790@qq.com>
2019-08-29 17:58:18 +0800

    取消报错监控系统

--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Raven from 'raven-js'
-import RavenVue from 'raven-js/plugins/vue'
-import settings from './settings'
 import { fetchPost, fetchGet } from './api'
 import router from './router/router'
 
@@ -10,12 +7,6 @@ Vue.config.productionTip = false
 process.env.NODE_ENV === 'development' && require('./mock')
 Vue.prototype.fetchGet = fetchGet
 Vue.prototype.fetchPost = fetchPost
-process.env.NODE_ENV !== 'development' && Raven
-    .config('http://f4a7081fa0ea4e5cb22ef6c106d9d5a4@127.0.0.1:9000/2', {
-        release: settings.RELEASE
-    })
-    .addPlugin(RavenVue, Vue)
-    .install()
 
 new Vue({
     router: router,
